a5785db7d449719cec810143da1902e50ef2a6c8
John Wakley <jwakley74@gmail.com>
2022-10-28 21:16:22 -0400

    new initial commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,5 @@
-#include "core/context.hpp"
-#include "core/window.hpp"
-#include "core/renderer.hpp"
-#include "utils/logger.hpp"
+#include <octo.hpp>
 
 int main(int argc, char** argv) {
-    LogDebug("main: starting", ENGINE_NAME_AND_VERSION);
-
-    octo::Context context{};
-    octo::Window window{&context};
-    octo::Renderer renderer{&context};
-
-    LogDebug("main: setting up event listeners");
-    window.onWindowResized([&](int width, int height) {
-        renderer.resize(width, height);
-    });
-
-    window.onKeyPressed(GLFW_KEY_ESCAPE, [&](int mods) {
-        LogDebug("main: onKeyPress: GLFW_KEY_ESCAPE");
-        window.shutdown();
-    });
-
-    LogDebug("main: Starting MainLoop");
-    while (window.pollEvents()) {
-        renderer.render();
-    }
-
     return 0;
 }
