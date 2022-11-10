624e179df97668e277dd2bcd2c1550d866cf436b
John Wakley <jwakley74@gmail.com>
2022-11-09 19:00:21 -0500

    clean slate, created octo and test projects

---
+++ octo/include/octo.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "defines.hpp"
+#include "logger.hpp"
+#include "utils.hpp"
---
+++ test/src/main.cpp
@@ -0,0 +1,11 @@
+#include <octo.hpp>
+
+int main(int argc, char **argv)
+{
+    log_debug("DEBUG LOG");
+    log_info("INFO LOG");
+    log_warn("WARNING LOG");
+    log_error("ERROR LOG");
+
+    return 0;
+}
