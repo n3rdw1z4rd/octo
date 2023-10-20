bb067c6b50e7b476fa19c5e15cacd9d4cace8791
John Wakley <john.wakley@icloud.com>
2023-10-20 16:21:36 -0400

    refined logger

--- octo/src/logger.c
+++ octo/src/logger.c
@@ -2,6 +2,8 @@
 #include "../include/core/time.h"
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include <stdarg.h>
 #include <string.h>
 
@@ -14,24 +16,25 @@ static bool win_console_initialized = false;
 #endif
 
 const char *_color_strings[6] = {
-    "\033[1;41m",
-    "\033[1;31m",
-    "\033[1;33m",
-    "\033[1;32m",
-    "\033[1;34m",
-    "\033[1;30m"};
+    "\033[1;41m",  // fatal, white on red, bold
+    "\033[1;31m",  // error, red on black, bold
+    "\033[1;33m",  // warn, yellow on black, bold
+    "\033[1;97m",  // info, white on black, bold
+    "\033[1;90m",  // debug, light gray on black, bold
+    "\033[0;90m"}; // trace, dark grey on black
 
 const char *_level_strings[6] = {
-    "[FATAL]",
-    "[ERROR]",
-    "[WARN] ",
-    "[INFO] ",
-    "[DEBUG]",
-    "[TRACE]"};
+    "[FTL]",
+    "[ERR]",
+    "[WRN]",
+    "[INF]",
+    "[DBG]",
+    "[TRC]"};
 
 void __log_message(LogLevel log_level, const char *msg_format, ...)
 {
-    bool is_error = (log_level < LOG_LEVEL_WARN);
+    time_t t = time(NULL);
+    struct tm tm = *localtime(&t);
 
     char args[OCTO_MAX_LOG_LENGTH];
     memset(args, 0, sizeof(args));
@@ -42,9 +45,14 @@ void __log_message(LogLevel log_level, const char *msg_format, ...)
     va_end(ptr);
 
     printf(
-        "%s%s%s %s%s %s%s\n",
+        "%s%d-%02d-%02d %02d:%02d:%02d%s %s%s %s%s\n",
         CYAN,
-        get_date_time_stamp(),
+        tm.tm_year + 1900,
+        tm.tm_mon + 1,
+        tm.tm_mday,
+        tm.tm_hour,
+        tm.tm_min,
+        tm.tm_sec,
         RESET,
         _color_strings[log_level],
         _level_strings[log_level],
